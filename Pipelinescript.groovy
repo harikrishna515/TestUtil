9404a70b6a3802bc5e28741245b52d2f1990e24f
Hari Garbham <M1040318@mindtree.com>
2019-02-08 11:56:01 +0530

    updated the groovy script

--- Pipelinescript.groovy
+++ Pipelinescript.groovy
@@ -1,24 +1,20 @@
 node('master'){
-def url =readProperties file: 'PropertiesFile.properties'
-stage('checkout') { 
-	echo "${url}"
-	def Var1= url.GIT_URL
-	echo "Var1=${Var1}"
-	git "${Var1}"
-}
+stage('git checkout') {
+git 'https://github.com/harikrishna515/DevOps-301Training.git'
+}  
 stage('Build & Compile') {    
-	sh 'mvn clean package'
+sh 'mvn clean package'
 }
 stage ('SonarQube Analysis'){
 sh 'mvn sonar:sonar'
 }
 stage ('Artifactory Deploy'){
 script {
-def server = Artifactory.newServer url: url.ARTIFACTORY_ID, username: url.username, password: 'url.password'
+def server = Artifactory.newServer url: 'http://localhost:8081/artifactory', username: 'admin', password: 'password'
 def uploadSpec = """{
   "files": [
     {
-      "pattern": "*.war",
+        "pattern": "*.war",
       "target": "lib-staging"
     }
  ]
@@ -28,7 +24,7 @@ server.upload(uploadSpec)
 }
 stage ('Deploy')
 {
-	sh 'sudo cp target/*.war url.DeploymentPath'
-	sh 'sudo ls -ltr url.DeploymentPath'
+	sh 'sudo cp target/*.war /home/devopsuser4/apache-tomcat-8.5.37/webapps'
+	sh 'sudo ls -ltr /home/devopsuser4/apache-tomcat-8.5.37/webapps
 }
 }
